d39e9c9e65292218a7f1f744f14a967bf7f61761
malof <m0follai@enib.fr>
2026-06-08 16:20:58 +0200

    Initial commit

---
+++ RecordWindow.py
@@ -0,0 +1,188 @@
+from PyQt6.QtCore import QTimer, Qt
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QApplication, QLabel, QSlider, QHBoxLayout
+from PyQt6.QtGui import QIcon
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+
+class RecordWindow(QWidget):
+    def __init__(self, target_path):
+        super().__init__()
+
+        self.target_path = target_path
+
+        self.setWindowTitle("Record sound")
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel(f"Recording to : {target_path.split('/')[-1]}")
+        layout.addWidget(self.label)
+
+        self.start_button = QPushButton()
+        self.start_button.setIcon(QIcon("icons/record.svg"))
+        self.stop_button = QPushButton()
+        self.stop_button.setIcon(QIcon("icons/stop.svg"))
+        self.play_button = QPushButton()
+        self.play_button.setIcon(QIcon("icons/play.svg"))
+        self.stop_button.setEnabled(False)
+
+        #Horizontal layout
+        self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(self.start_button)
+        self.buttons_layout.addWidget(self.stop_button)
+        self.buttons_layout.addWidget(self.play_button)
+        layout.addLayout(self.buttons_layout)
+
+
+
+        self.setLayout(layout)
+
+        self.start_button.clicked.connect(self.start_countdown)
+        self.stop_button.clicked.connect(self.stop_recording)
+        self.play_button.clicked.connect(self.play_recording)
+
+        self.stream = None
+        self.frames = []
+        self.sample_rate = 44100
+
+        self.time_elapsed = 0
+
+        self.timer_label = QLabel("00:00")
+        layout.addWidget(self.timer_label)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_timer)
+
+        self.amp_label = QLabel("Amplification :")
+        self.amp_label.setEnabled(False)
+        layout.addWidget(self.amp_label)
+        amp_layout = QHBoxLayout()
+        self.amp_level = QLabel("1")
+        self.amp_slider = QSlider(Qt.Orientation.Horizontal)
+        self.amp_slider.setRange(0, 50)
+        self.amp_slider.setValue(1)
+        self.amp_slider.valueChanged.connect(
+            lambda value: self.amp_level.setText(str(value))
+        )
+        self.amp_slider.setEnabled(False)
+        self.amp_level.setEnabled(False)
+        amp_layout.addWidget(self.amp_level)
+        amp_layout.addWidget(self.amp_slider)
+        layout.addLayout(amp_layout)
+
+        self.save_button = QPushButton("Save")
+        layout.addWidget(self.save_button)
+        self.save_button.clicked.connect(self.save_audio)
+        self.save_button.setEnabled(False)
+
+        #Set heigh and width
+        self.setFixedSize(400, 200)
+
+        self.countdown_value = 3
+
+        self.countdown_timer = QTimer()
+        self.countdown_timer.timeout.connect(self.update_countdown)
+
+    def start_countdown(self):
+        self.countdown_value = 3
+
+        self.timer_label.setText(str(self.countdown_value))
+
+        self.start_button.setEnabled(False)
+        self.play_button.setEnabled(False)
+        self.save_button.setEnabled(False)
+        self.amp_slider.setEnabled(False)
+        self.amp_level.setEnabled(False)
+        self.amp_label.setEnabled(False)
+
+        self.countdown_timer.start(1000)
+
+    def update_countdown(self):
+        self.countdown_value -= 1
+
+        if self.countdown_value > 0:
+            self.timer_label.setText(str(self.countdown_value))
+        else:
+            self.countdown_timer.stop()
+
+            self.timer_label.setText("00:00")
+
+            self.start_recording()
+
+    def start_recording(self):
+        self.stop_button.setEnabled(True)
+
+        self.time_elapsed = 0
+        self.timer_label.setText("00:00")
+
+        self.timer.start(1000)
+
+        self.frames = []
+
+        self.stream = sd.InputStream(
+            samplerate=self.sample_rate,
+            channels=1,
+            callback=self.audio_callback
+        )
+
+        self.stream.start()
+
+        print("Recording...")
+
+    def stop_recording(self):
+        if self.stream is None:
+            return
+
+        self.stop_button.setEnabled(False)
+        self.start_button.setEnabled(True)
+        self.save_button.setEnabled(True)
+        self.amp_slider.setEnabled(True)
+        self.amp_level.setEnabled(True)
+        self.amp_label.setEnabled(True)
+        self.play_button.setEnabled(True)
+
+        self.timer.stop()
+
+        self.stream.stop()
+        self.stream.close()
+
+    def play_recording(self):
+        audio = np.concatenate(self.frames, axis=0)
+        audio = audio * self.amp_slider.value()
+        sd.play(audio, self.sample_rate)
+
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            print(status)
+
+        self.frames.append(indata.copy())
+
+    def update_timer(self):
+        self.time_elapsed += 1
+
+        minutes = self.time_elapsed // 60
+        seconds = self.time_elapsed % 60
+
+        self.timer_label.setText(f"{minutes:02}:{seconds:02}")
+
+    def save_audio(self):
+        audio = np.concatenate(self.frames, axis=0)
+
+        audio = audio * self.amp_slider.value()
+
+        audio = np.clip(audio, -1.0, 1.0)
+
+        sf.write(
+            self.target_path,
+            audio,
+            self.sample_rate
+        )
+        print("Audio saved successfully.")
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = RecordWindow("test.wav")
+    window.show()
+    app.exec()
---
+++ SoundChanger.py
@@ -0,0 +1,267 @@
+from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QListWidget, QTreeWidget, \
+    QTreeWidgetItem, QMenu, QLabel
+import os
+import zipfile
+import shutil
+
+from RecordWindow import RecordWindow
+
+class SoundChanger(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("MOHAA Sound Changer")
+        self.folder_path = None
+        self.pack_name = '/Pak3.pk3'
+        self.extract_path = '/temp'
+
+        self.main_layout = QVBoxLayout()
+
+        button = QPushButton("Select MOHAA folder")
+        button.clicked.connect(self.select_folder)
+        self.main_layout.addWidget(button)
+
+        self.setLayout(self.main_layout)
+
+        self.player = QMediaPlayer()
+        self.audio_output = QAudioOutput()
+
+        self.player.setAudioOutput(self.audio_output)
+
+        self.setGeometry(0, 0, 800, 600)
+
+    def select_folder(self):
+        self.folder_path = QFileDialog.getExistingDirectory(self, "Select MOHAA folder")
+        if self.folder_path:
+            print("Selected folder:", self.folder_path)
+        else:
+            print("No folder selected.")
+            return
+
+        if not os.path.exists(self.folder_path + '/backup'):
+            os.makedirs(self.folder_path + '/backup')
+
+        if os.path.exists(self.folder_path+self.pack_name):
+            self.pak_path = self.folder_path + self.pack_name
+            self.bkp_path = self.folder_path + '/backup' + self.pack_name
+            self.sounds_path = self.folder_path + self.extract_path
+        elif os.path.exists(self.folder_path+'/main'+self.pack_name):
+            self.pak_path = self.folder_path + '/main' + self.pack_name
+            self.bkp_path = self.folder_path + '/main/backup' + self.pack_name
+            self.sounds_path = self.folder_path + '/main' + self.extract_path
+        else:
+            print("No files found in the specified folder.")
+            return
+
+        shutil.copyfile(self.pak_path, self.bkp_path)
+        self.extract_files(self.pak_path, self.sounds_path)
+
+        sounds = {}
+
+        sound_root = os.path.join(self.sounds_path, "sound")
+
+        for root, dirs, files in os.walk(sound_root):
+
+            category = os.path.relpath(root, sound_root)
+
+            if category == ".":
+                category = "root"
+
+            for file in files:
+                if file.lower().endswith((".wav", ".mp3")):
+
+                    full_path = os.path.join(root, file)
+
+                    if category not in sounds:
+                        sounds[category] = []
+
+                    sounds[category].append(
+                        (file, full_path)
+                    )
+
+        self.show_sounds(sounds)
+
+    def extract_files(self, zip_file, extract_path):
+        print("Extracting files...")
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+            print("Files extracted successfully.")
+
+    def compress_files(self, folder_path, zip_file):
+        with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED) as zipf:
+            try:
+
+                for root, dirs, files in os.walk(folder_path):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+
+                        archive_path = os.path.relpath(file_path, folder_path)
+
+                        zipf.write(file_path, archive_path)
+
+                return 'ok'
+            except Exception as e:
+                print(f"Error compressing files: {e}")
+                return e
+
+    def show_sounds(self, sounds):
+        while self.main_layout.count():
+            item = self.main_layout.takeAt(0)
+            if item.widget():
+                item.widget().deleteLater()
+
+        tree = QTreeWidget()
+        tree.setHeaderLabel("Sounds")
+
+        for category, files in sounds.items():
+
+            category_item = QTreeWidgetItem([category])
+
+            for name, path in files:
+                sound_item = QTreeWidgetItem([name])
+
+                sound_item.setData(
+                    0,
+                    Qt.ItemDataRole.UserRole,
+                    path
+                )
+
+                category_item.addChild(sound_item)
+
+            tree.addTopLevelItem(category_item)
+
+        tree.itemDoubleClicked.connect(self.play_sound)
+
+        tree.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        tree.customContextMenuRequested.connect(self.show_context_menu)
+
+        self.tree = tree
+
+        self.save_button = QPushButton("Save")
+        self.save_button.clicked.connect(self.save_changes)
+
+        self.status_label = QLabel()
+        self.status_label.setVisible(False)
+        self.status_label.setStyleSheet(
+            "QLabel { color: green; }"
+        )
+
+        self.main_layout.addWidget(tree)
+        self.main_layout.addWidget(self.save_button)
+        self.main_layout.addWidget(self.status_label)
+
+    def show_context_menu(self, pos):
+        item = self.tree.itemAt(pos)
+
+        if item is None:
+            return
+
+        path = item.data(0, Qt.ItemDataRole.UserRole)
+
+        if path is None:
+            return
+
+        menu = QMenu()
+
+        play_action = menu.addAction("Play")
+        replace_action = menu.addAction("Replace")
+        open_action = menu.addAction("Open folder")
+        record_action = menu.addAction("Record")
+
+        action = menu.exec(
+            self.tree.viewport().mapToGlobal(pos)
+        )
+
+        if action == play_action:
+            self.play_sound(item, 0)
+
+        elif action == replace_action:
+            self.replace_sound(path)
+
+        elif action == open_action:
+            self.open_folder(path)
+
+        elif action == record_action:
+            self.record_sound(path)
+
+    def replace_sound(self, old_path):
+        new_file, _ = QFileDialog.getOpenFileName(
+            self,
+            "Choisir un nouveau son",
+            "",
+            "Audio (*.wav *.mp3)"
+        )
+
+        #Convert to 1 channel
+        if new_file.endswith(".wav"):
+            new_file = new_file.replace(".wav", "_1.wav")
+            os.system(f"ffmpeg -i {new_file} -ac 1 {new_file}")
+            new_file = new_file.replace("_1.wav", ".wav")
+        elif new_file.endswith(".mp3"):
+            new_file = new_file.replace(".mp3", "_1.mp3")
+            os.system(f"ffmpeg -i {new_file} -ac 1 {new_file}")
+            new_file = new_file.replace("_1.mp3", ".wav")
+
+        if not new_file:
+            return
+
+        shutil.copy2(new_file, old_path)
+
+        print(f"Replaced : {old_path}")
+
+    def open_folder(self, path):
+        os.startfile(os.path.dirname(path))
+
+    def play_sound(self, item, column):
+        path = item.data(0, Qt.ItemDataRole.UserRole)
+
+        if path is None:
+            return
+
+        self.player.setSource(QUrl.fromLocalFile(path))
+        self.player.play()
+
+    def record_sound(self, path):
+        self.record_window = RecordWindow(path)
+        self.record_window.show()
+
+    def save_changes(self):
+        print("Backing up...")
+        self.backup(
+            self.pak_path,
+            self.bkp_path
+        )
+        print("Saving changes...")
+        print(self.sounds_path)
+        if 'main' in self.folder_path:
+            save_path = self.folder_path + self.pack_name
+        else:
+            save_path = self.folder_path + '/main' + self.pack_name
+        result = self.compress_files(self.sounds_path, save_path)
+        if result == 'ok':
+            self.status_label.setText("Changes saved successfully.")
+        else:
+            self.status_label.setText(f"Error saving changes: {result}")
+        self.status_label.setVisible(True)
+
+    def backup(self, pak_path, bkp_path):
+        if os.path.exists(bkp_path):
+            i=0
+            bkp_path = bkp_path.replace(self.pack_name, f"/{self.pack_name[1:-4]}_backup{i}.pk3")
+            while True:
+                bkp_path = bkp_path.replace(f"backup{i}", f"backup{i+1}")
+                if not os.path.exists(bkp_path):
+                    break
+                i+=1
+
+        shutil.copyfile(pak_path, bkp_path)
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = SoundChanger()
+    window.show()
+
+    app.exec()
